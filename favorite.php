67f91e90a5c713ad765ebd257b27b13995fd780b
githubYubella <anmpbella@gmail.com>
2022-04-03 06:16:36 +0700

    fav php

---
+++ favorite.php
@@ -0,0 +1,27 @@
+[
+    {
+        "id":2,
+        "name":"Kost Puspa Dewi",
+        "jenis":"Perempuan",
+        "fasilitas":"Kamar Kosongan",
+        "harga":"Rp 600.000",
+        "photoUrl":"https://cyn9.files.wordpress.com/2010/04/picture-016.jpg",
+        "alamat":"Jl. Rungkut Mejoyo Utara XI/36. Kode Pos, : 4478",
+        "rekening":"445 800 967 445",
+        "atas_nama":"Melani" 
+        },
+        {
+        "id":6,
+        "name":"Asyta Kost",
+        "jenis":"Pria",
+        "fasilitas":"AC, KM Dalam, Wi-Fi, Laundry",
+        "harga":"Rp 1000.000",
+        "photoUrl":"https://nomplok.com/wp-content/uploads/2020/09/Kost-AC-Parkir-sangat-Luas-313x234.jpg",
+        "alamat":"Jl.Semolowaru Gang IV No.7",
+        "rekening":"772 665 995 441",
+        "atas_nama":"Steven Reyner"
+        
+
+        }
+        
+]
