86b50ba98e71f8dfe0583136dd43a91c4a264bb4
githubYubella <anmpbella@gmail.com>
2022-04-03 05:34:39 +0700

    php

---
+++ data_user.php
@@ -0,0 +1,8 @@
+{
+    "username": "yubella",
+    "password": "123",
+    "name": "Margareta Yubella",
+    "email": "s160419107@student.ubaya.ac.id",
+    "tanggal_lahir": "5 Maret 1998",
+    "photo": "https://i.pinimg.com/736x/c6/de/4f/c6de4fbc92c32c25dd90c41884968d63--die-minions-minion-s.jpg"
+}
---
+++ listKost.php
@@ -0,0 +1,71 @@
+
+    [
+        {"id":1,
+        "name":"Kost Pelangi",
+        "jenis":"Perempuan",
+        "fasilitas":"Laundry, meja,kursi,dll",
+        "harga":"Rp 800.000",
+        "photoUrl":"https://pindahlubang.com/wp-content/uploads/2021/04/Biaya-Bangun-Rumah-Kost-2-Lantai.jpg",
+        "alamat":"Jl. Rungkut Mejoyo Selatan X/36. Kode Pos, : 60292",
+        "rekening":"008 559 224 77",
+        "atas_nama":"Budina"
+        },
+        {
+        "id":2,
+        "name":"Kost Puspa Dewi",
+        "jenis":"Perempuan",
+        "fasilitas":"Kamar Kosongan",
+        "harga":"Rp 600.000",
+        "photoUrl":"https://cyn9.files.wordpress.com/2010/04/picture-016.jpg",
+        "alamat":"Jl. Rungkut Mejoyo Utara XI/36. Kode Pos, : 4478",
+        "rekening":"445 800 967 445",
+        "atas_nama":"Melani"
+        },
+        {
+        "id":3,
+        "name":"Kost Shangri La",
+        "jenis":"Pria",
+        "fasilitas":"Tempat tidur, Kipas angin, dll",
+        "harga":"Rp 900.000",
+        "photoUrl":"https://www.sewakost.com/files/03-2021/ad49854/kost-pria-di-cempaka-baru-jakarta-pusat-dekat-1700542706.JPG",
+        "alamat":"Jl. Rungkut XI/3. Kode Pos, : 110893",
+        "rekening":"778 988 322 445",
+        "atas_nama":"Hartono Mulyo"
+        },
+        {
+        "id":4,
+        "name":"Teras Cotta",
+        "jenis":"Pria",
+        "fasilitas":"AC, Full Furnish",
+        "harga":"Rp 1.500.000",
+        "photoUrl":"https://acryliczone.id/wp-content/uploads/2021/01/PN08-Papan-Nama-Kamar-Kost-Muslim-Teras-Cotta.png",
+        "alamat":"Jl. Kalangan RT 17 NO 17 Baturetno. Kode Pos : 558641",
+        "rekening":"085 227 449 66",
+        "atas_nama":"Wildan Arifsyah"
+        },
+        {
+        "id":5,
+        "name":"Kost Firdaus",
+        "jenis":"Perempuan",
+        "fasilitas":"non-AC, KM Dalam, Kosongan",
+        "harga":"Rp 750.000",
+        "photoUrl":"https://www.sewakost.com/files/11-2019/ad27837/kost-kinanti-masih-kosong-222910279_large.jpg",
+        "alamat":"Jl. Panjang Jiwo No.13 Kode Pos : 88546",
+        "rekening":"555 998 334 552",
+        "atas_nama":"Martiana Sulastri"
+        },
+        {
+        "id":6,
+        "name":"Asyta Kost",
+        "jenis":"Pria",
+        "fasilitas":"AC, KM Dalam, Wi-Fi, Laundry",
+        "harga":"Rp 1000.000",
+        "photoUrl":"https://nomplok.com/wp-content/uploads/2020/09/Kost-AC-Parkir-sangat-Luas-313x234.jpg",
+        "alamat":"Jl.Semolowaru Gang IV No.7",
+        "rekening":"772 665 995 441",
+        "atas_nama":"Steven Reyner"
+        
+
+        }
+        
+    ]
